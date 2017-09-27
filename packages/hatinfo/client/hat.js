071480ad53e4e83ba1584a97f09c9dbe6eb93ac6
Lars Wolter <lars.wolter@geomagic.world>
2017-09-27 22:45:51 +0200

    fixed hatinfo package

--- packages/hatinfo/client/hat.js
+++ packages/hatinfo/client/hat.js
@@ -1,178 +1,179 @@
-import {AutoForm} from 'meteor/ultisite:autoform';
+import { AutoForm } from 'meteor/ultisite:autoform';
 
 FlowRouter.route('/hat/:_id?/:confirmed?', {
-    name: "hatinfo",
-    action: function () {
-        UltiSite.baseLayoutData.set( {
-            content: "hatInfos"
-        });
-    }
+  name: "hatinfo",
+  action () {
+    UltiSite.baseLayoutData.set({
+      content: "hatInfos",
+    });
+  },
 });
 
 FlowRouter.route('/hat_confirm/:accessKey?', {
-    name: "hatinfoConfirm",
-    triggersEnter: [function (context, redirect) {
-        const accessKey = context.params.accessKey;
-        if (accessKey)
-            Meteor.call('hatConfirmParticipant', accessKey);
-        redirect(`/hat/${accessKey}/confirmed`);
-    }]
+  name: "hatinfoConfirm",
+  triggersEnter: [function (context, redirect) {
+    const accessKey = context.params.accessKey;
+    if (accessKey) { Meteor.call('hatConfirmParticipant', accessKey); }
+    redirect(`/hat/${accessKey}/confirmed`);
+  }],
 });
 
 Meteor.startup(function () {
-    UltiSite.registerAdminPageTemplate('hatInfoSettings', 'HAT Anmeldung');
+  UltiSite.registerAdminPageTemplate('hatInfoSettings', 'HAT Anmeldung');
 });
 
 
 Template.hatInfoSettings.helpers({
-    wikiPages: function () {
-        return Session.get("wikiPageNames");
-    },
-    wikiPageName: function (id) {
-        var name = "";
-        if (Session.get("wikiPageNames"))
-            Session.get("wikiPageNames").forEach(function (page) {
-                if (page._id == id)
-                    name = page.name;
-            });
-        return name;
+  wikiPages () {
+    return Session.get("wikiPageNames");
+  },
+  wikiPageName (id) {
+    let name = "";
+    if (Session.get("wikiPageNames")) {
+      Session.get("wikiPageNames").forEach(function (page) {
+        if (page._id == id) { name = page.name; }
+      });
     }
+    return name;
+  },
 });
 
 Template.hatInfos.onCreated(function () {
-    this.limit = new ReactiveVar(25);
-    this.filter = new ReactiveVar('');
-    this.autorun(()=>{
-        this.subscribe('hatParticipant', FlowRouter.getParam('_id'));
-        this.subscribe('hatParticipants',this.limit.get(),this.filter.get());
-    });
+  this.limit = new ReactiveVar(25);
+  this.filter = new ReactiveVar('');
+  this.autorun(() => {
+    this.subscribe('hatParticipant', FlowRouter.getParam('_id'));
+    this.subscribe('hatParticipants', this.limit.get(), this.filter.get());
+  });
 });
 function hatSort() {
-    const sort = {};
-    if (UltiSite.settings().hatSort)
-        sort[UltiSite.settings().hatSort] = 1;
-    sort.createdAt = 1;
-    return sort;
+  const sort = {};
+  if (UltiSite.settings().hatSort) { sort[UltiSite.settings().hatSort] = 1; }
+  sort.createdAt = 1;
+  return sort;
 }
 
 Template.hatInfos.helpers({
-    allParticipants() {
-        if (Template.instance().filter.get())
-            return UltiSite.HatInfo.HatParticipants.find({
-                $or: [
+  allParticipants() {
+    if (Template.instance().filter.get()) {
+      return UltiSite.HatInfo.HatParticipants.find({
+        $or: [
                     { name: new RegExp(Template.instance().filter.get(), 'i') },
-                    { email: new RegExp(Template.instance().filter.get(), 'i') }
-                ]
-            }, { sort: hatSort() });
-        else
-            return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort() });
-    },
-    activeFilter() {
-        return !!Template.instance().filter.get();
-    },
-    hatParticipants() {
-        return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), limit: Number(UltiSite.settings().hatNumPlayers) });
-    },
-    hatWaiting() {
-        return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), skip: Number(UltiSite.settings().hatNumPlayers) });
-    },
-    payedParticipants() {
-        return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), limit: Number(UltiSite.settings().hatNumPlayers) }).fetch().filter((p) => {
-            return moment(p.payed).isBefore(moment());
-        }).length;
-    },
-    payedWaiting() {
-        return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), skip: Number(UltiSite.settings().hatNumPlayers) }).fetch().filter((p) => {
-            return moment(p.payed).isBefore(moment());
-        }).length;
-    },
-    emails() {
-        return UltiSite.HatInfo.HatParticipants.find().map(p => p.email).join(',');
-    },
-    emailsParticipate() {
-        return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), limit: Number(UltiSite.settings().hatNumPlayers) }).map(p => p.email).join(',');
-    },
-    emailsWaiting() {
-        return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), skip: Number(UltiSite.settings().hatNumPlayers) }).map(p => p.email).join(',');
-    },
-    activeEntry() {
-        if (FlowRouter.getParam('_id'))
-            return UltiSite.HatInfo.HatParticipants.findOne({ accessKey: FlowRouter.getParam('_id') });
+                    { email: new RegExp(Template.instance().filter.get(), 'i') },
+   ],
+      }, { sort: hatSort() });
     }
+    return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort() });
+  },
+  activeFilter() {
+    return !!Template.instance().filter.get();
+  },
+  hatParticipants() {
+    return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), limit: Number(UltiSite.settings().hatNumPlayers) });
+  },
+  hatWaiting() {
+    return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), skip: Number(UltiSite.settings().hatNumPlayers) });
+  },
+  payedParticipants() {
+    return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), limit: Number(UltiSite.settings().hatNumPlayers) }).fetch().filter(p => moment(p.payed).isBefore(moment())).length;
+  },
+  payedWaiting() {
+    return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), skip: Number(UltiSite.settings().hatNumPlayers) }).fetch().filter(p => moment(p.payed).isBefore(moment())).length;
+  },
+  emails() {
+    return UltiSite.HatInfo.HatParticipants.find().map(p => p.email).join(',');
+  },
+  emailsParticipate() {
+    return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), limit: Number(UltiSite.settings().hatNumPlayers) }).map(p => p.email).join(',');
+  },
+  emailsWaiting() {
+    return UltiSite.HatInfo.HatParticipants.find({}, { sort: hatSort(), skip: Number(UltiSite.settings().hatNumPlayers) }).map(p => p.email).join(',');
+  },
+  activeEntry() {
+    if (FlowRouter.getParam('_id')) { return UltiSite.HatInfo.HatParticipants.findOne({ accessKey: FlowRouter.getParam('_id') }); }
+  },
 });
 
 Template.hatParticipant.helpers({
-    strengthPercent: function () {
-        return (Number(this.strength) + 1) * 10;
-    },
-    accessKey: function () {
-        return FlowRouter.getParam('_id');
-    },
-    hasPayed() {
-        return moment(this.payed).isBefore(moment());
-    },
-    activeEntry() {
-        return this.accessKey && this.accessKey === FlowRouter.getParam('_id');
-    }
+  strengthPercent () {
+    return (Number(this.strength) + 1) * 10;
+  },
+  accessKey () {
+    return FlowRouter.getParam('_id');
+  },
+  hasPayed() {
+    return moment(this.payed).isBefore(moment());
+  },
+  activeEntry() {
+    return this.accessKey && this.accessKey === FlowRouter.getParam('_id');
+  },
 });
 Template.hatParticipant.events({
-    'click .action-remove': function (evt) {
-        evt.preventDefault();
-        UltiSite.confirmDialog(`Willst du ${this.name} wirklich austragen?`, () => {
-            Meteor.call('hatRemoveParticipation', this.accessKey, UltiSite.userFeedbackFunction("Vom HAT austragen"));
-            FlowRouter.go("hatinfo");
-        });
-    },
-    'click .action-payed': function (evt) {
-        evt.preventDefault();
-        Meteor.call('hatParticipationPayed', this.accessKey, UltiSite.userFeedbackFunction("Zahlungsstatus ändern", evt.currentTarget));
-    }
+  'click .action-remove' (evt) {
+    evt.preventDefault();
+    UltiSite.confirmDialog(`Willst du ${this.name} wirklich austragen?`, () => {
+      Meteor.call('hatRemoveParticipation', this.accessKey, UltiSite.userFeedbackFunction("Vom HAT austragen"));
+      FlowRouter.go("hatinfo");
+    });
+  },
+  'click .action-participate' (evt) {
+    evt.preventDefault();
+    UltiSite.showModal('hatParticipateDialog', this);
+  },
+  'click .action-payed' (evt) {
+    evt.preventDefault();
+    Meteor.call('hatParticipationPayed', this.accessKey, UltiSite.userFeedbackFunction("Zahlungsstatus ändern", evt.currentTarget));
+  },
 });
 
 Template.hatInfos.events({
-    'keyup .participant-filter': function (evt, tmpl) {
-        tmpl.filter.set(tmpl.$(evt.currentTarget).val());
-    },
-    'click .action-participate': function (evt) {
-        FlowRouter.go('hatinfo');
-    },
-    'click .participant-entry': function (evt) {
-        evt.preventDefault();
-        if (FlowRouter.getParam('_id') === this.accessKey)
-            FlowRouter.go('hatinfo');
-        else
-            FlowRouter.go('hatinfo', { _id: this.accessKey });
-    }
+  'keyup .participant-filter' (evt, tmpl) {
+    tmpl.filter.set(tmpl.$(evt.currentTarget).val());
+  },
+  'click .action-participate' (evt) {
+    evt.preventDefault();
+    UltiSite.showModal('hatParticipateDialog');
+  },
+  'click .participant-entry' (evt) {
+    evt.preventDefault();
+    if (FlowRouter.getParam('_id') === this.accessKey) { FlowRouter.go('hatinfo'); } else { FlowRouter.go('hatinfo', { _id: this.accessKey }); }
+  },
 });
 
 Template.hatParticipateDialog.helpers({
-    existing() {
-        if (FlowRouter.getParam('_id'))
-            return UltiSite.HatInfo.HatParticipants.findOne({ accessKey: FlowRouter.getParam('_id') });
-    },
-    error() {
-        return Session.get('hatParticipateError');
-    },
-    method() {
-        if (FlowRouter.getParam('_id'))
-            return 'hatUpdateParticipation';
-        return 'hatParticipate';
-    }
+  existing() {
+    if (FlowRouter.getParam('_id')) {
+      return UltiSite.HatInfo.HatParticipants.findOne({ accessKey: FlowRouter.getParam('_id') }); 
+}
+  },
+  schema() {
+    return UltiSite.HatInfo.schema;
+  },
+  error() {
+    return Session.get('hatParticipateError');
+  },
+  method() {
+    if (FlowRouter.getParam('_id')) {
+      return 'hatUpdateParticipation'; 
+}
+    return 'hatParticipate';
+  },
 });
 
 AutoForm.hooks({
-    hatParticipantForm: {
-        onSuccess: function (formType, result) {
-            console.log(result);
-            $('#hatParticipateDialog').modal('hide');
-            if (result !== 'updated')
-                $('#hatParticipateSuccess').modal('show');
-        },
-        onError: function (formType, error) {
-            console.log('hatParticipantForm:', error);
-            $('#hatParticipateDialog').modal('hide');
-            Session.set('hatParticipateError', error.message);
-            $('#hatParticipateError').modal('show');
-        }
-    }
+  hatParticipantForm: {
+    onSuccess (formType, result) {
+      console.log(result);
+      UltiSite.hideModal();
+      if (result !== 'updated') {
+        UltiSite.showModal('hatParticipateDialogSuccess');
+      }
+    },
+    onError (formType, error) {
+      console.log('hatParticipantForm:', error);
+      UltiSite.hideModal();
+      Session.set('hatParticipateError', error.message);
+      UltiSite.showModal('hatParticipateDialogError');
+    },
+  },
 });
--- packages/hatinfo/server/hat.js
+++ packages/hatinfo/server/hat.js
@@ -2,61 +2,58 @@
 
 Meteor.startup(function () {
   UltiSite.HatInfo.HatParticipants.find({ payed: { $exists: false } }).forEach((elem) => {
-      UltiSite.HatInfo.HatParticipants.update(elem._id, { $set: { payed: moment(elem.createdAt).clone().add(10, 'years').toDate() } });
-    });
+    UltiSite.HatInfo.HatParticipants.update(elem._id, { $set: { payed: moment(elem.createdAt).clone().add(10, 'years').toDate() } });
+  });
 });
 Meteor.methods({
   hatConfirmParticipant(accessKey) {
-      UltiSite.HatInfo.HatParticipants.update({ accessKey }, { $set: { confirmed: true } });
-    },
+    UltiSite.HatInfo.HatParticipants.update({ accessKey }, { $set: { confirmed: true } });
+  },
   hatParticipate(participant) {
-      participant.createdAt = new Date();
-      participant.modifiedAt = new Date();
-      participant.payed = moment().add(10, 'years').toDate();
-      participant.accessKey = Random.id(34);
-      if (UltiSite.HatInfo.HatParticipants.findOne({ email: participant.email }))
-          throw new Meteor.Error('Teilnehmer mit dieser E-Mail exisitiert schon!');
-      UltiSite.HatInfo.HatParticipants.insert(participant);
-      UltiSite.Mail.send([participant.email], "Anmeldung beim " + UltiSite.settings().hatName + ' bestätigen',
-            `Hallo ${participant.name}, \n\n bitte öffne den folgenden Link, um deine Anmeldung beim HitHat zu bestätigen.\n\n` +
-            Meteor.absoluteUrl("hat_confirm/" + participant.accessKey) +
-            "\n\nViele Grüße\n" + UltiSite.settings().teamname);
-      return 'inserted';
-    },
+    participant.createdAt = new Date();
+    participant.modifiedAt = new Date();
+    participant.payed = moment().add(10, 'years').toDate();
+    participant.accessKey = Random.id(34);
+    participant.hatId = UltiSite.settings().hatId;
+    if (UltiSite.HatInfo.HatParticipants.findOne({ email: participant.email })) { throw new Meteor.Error('Teilnehmer mit dieser E-Mail exisitiert schon!'); }
+    UltiSite.HatInfo.HatParticipants.insert(participant);
+    UltiSite.Mail.send([participant.email], `Anmeldung beim ${UltiSite.settings().hatName} bestätigen`,
+            `Hallo ${participant.name}, \n\n bitte öffne den folgenden Link, um deine Anmeldung beim HitHat zu bestätigen.\n\n${
+            Meteor.absoluteUrl(`hat_confirm/${participant.accessKey}`)
+            }\n\nViele Grüße\n${UltiSite.settings().teamname}`);
+    return 'inserted';
+  },
   hatParticipationPayed(accessKey) {
-      let part = UltiSite.HatInfo.HatParticipants.findOne({ accessKey });
-      if (!part)
-          throw new Meteor.Error('access-denied', 'Zahlung nicht erlaubt');
-      if (!Roles.userIsInRole(this.userId, ['admin', 'hatAdmin']))
-          throw new Meteor.Error('access-denied', 'Zahlung nicht erlaubt');
+    const part = UltiSite.HatInfo.HatParticipants.findOne({ accessKey });
+    if (!part) { throw new Meteor.Error('access-denied', 'Zahlung nicht erlaubt'); }
+    if (!Roles.userIsInRole(this.userId, ['admin', 'hatAdmin'])) { throw new Meteor.Error('access-denied', 'Zahlung nicht erlaubt'); }
 
-      UltiSite.HatInfo.HatParticipants.update(part._id, { $set: { payed: new Date() } });
-      return 'payed';
-    },
+    UltiSite.HatInfo.HatParticipants.update(part._id, { $set: { payed: new Date() } });
+    return 'payed';
+  },
   hatUpdateParticipation(participant) {
-      let part = UltiSite.HatInfo.HatParticipants.findOne({ accessKey: participant.accessKey });
-      if (!part)
-          throw new Meteor.Error('access-denied', 'Änderung nicht erlaubt');
-      participant.modifiedAt = new Date();
+    const part = UltiSite.HatInfo.HatParticipants.findOne({ accessKey: participant.accessKey });
+    if (!part) { throw new Meteor.Error('access-denied', 'Änderung nicht erlaubt'); }
+    participant.modifiedAt = new Date();
 
-      UltiSite.HatInfo.HatParticipants.update(part._id, { $set: _.pick(participant, 'name', 'city', 'hometeam', 'strength', 'modifiedAt') });
-      return 'updated';
-    },
+    UltiSite.HatInfo.HatParticipants.update(part._id, { $set: _.pick(participant, 'name', 'city', 'hometeam', 'strength', 'modifiedAt') });
+    return 'updated';
+  },
   hatRemoveParticipation(accessKey) {
-      let part = UltiSite.HatInfo.HatParticipants.findOne({ accessKey });
-      if (!part)
-          throw new Meteor.Error('access-denied', 'Änderung nicht erlaubt');
-      UltiSite.HatInfo.HatParticipants.remove(part._id);
-      return 'removed';
-    }
+    const part = UltiSite.HatInfo.HatParticipants.findOne({ accessKey });
+    if (!part) { throw new Meteor.Error('access-denied', 'Änderung nicht erlaubt'); }
+    UltiSite.HatInfo.HatParticipants.remove(part._id);
+    return 'removed';
+  },
 });
 
-Meteor.publish('hatParticipants', function (limit,search) {
+Meteor.publish('hatParticipants', function (limit, search) {
   const sort = {};
-  if (UltiSite.settings().hatSort)
-      sort[UltiSite.settings().hatSort] = 1;
+  if (UltiSite.settings().hatSort) { sort[UltiSite.settings().hatSort] = 1; }
   sort.createdAt = 1;
-  const filter={};
+  const filter = {
+    hatId: UltiSite.settings().hatId || undefined,
+  };
     /*
     if(search && search.trim())
         filter.$or= [
@@ -64,41 +61,38 @@ Meteor.publish('hatParticipants', function (limit,search) {
             { email: new RegExp(search, 'i') }
         ];
     */
-  if (Roles.userIsInRole(this.userId, ['admin', 'hatAdmin']))
-      return UltiSite.HatInfo.HatParticipants.find(filter,{sort});
+  if (Roles.userIsInRole(this.userId, ['admin', 'hatAdmin'])) { return UltiSite.HatInfo.HatParticipants.find(filter, { sort }); }
 
   return UltiSite.HatInfo.HatParticipants.find(filter, {
-      sort,
-      fields: {
-          name: 1,
-          strength: 1,
-          city: 1,
-          hometeam: 1,
-          createdAt: 1,
-          confirmed: 1,
-          payed: 1
-        }
-    });
+    sort,
+    fields: {
+      name: 1,
+      strength: 1,
+      city: 1,
+      hometeam: 1,
+      createdAt: 1,
+      confirmed: 1,
+      payed: 1,
+    },
+  });
 });
 Meteor.publish('hatParticipant', function (accessKey) {
   return UltiSite.HatInfo.HatParticipants.find({ accessKey });
 });
 
 WebApp.connectHandlers.use("/_hatInfoExport", function (req, res, next) {
-  let query = Npm.require('url').parse(req.url, true).query;
-  let user = Meteor.users.findOne({ 'profile.downloadToken': query.downloadToken });
+  const query = Npm.require('url').parse(req.url, true).query;
+  const user = Meteor.users.findOne({ 'profile.downloadToken': query.downloadToken });
   if (!user) {
-      res.writeHead(403);
-      res.end();
-      return;
-    }
+    res.writeHead(403);
+    res.end();
+    return;
+  }
 
-  let csv = _.without(UltiSite.HatInfo.schema._schemaKeys, 'accessKey').join(';') + '\n\r';
-  csv += UltiSite.HatInfo.HatParticipants.find().map((p) => {
-      return _.without(UltiSite.HatInfo.schema._schemaKeys, 'accessKey').map((key) => {
-          return '"' + String(p[key]).replace(/"/g, '\'') + '"';
-        }).join(';');
-    }).join('\n\r');
-  res.writeHead(200, { "content-type": "text/csv", "content-disposition": "attachment; filename=\"participants-" + UltiSite.settings().hatName + ".csv\"" });
-  res.end(csv + '\n\r');
+  let csv = `${_.without(UltiSite.HatInfo.schema._schemaKeys, 'accessKey').join(';')}\n\r`;
+  csv += UltiSite.HatInfo.HatParticipants.find({
+    hatId: UltiSite.settings().hatId || undefined,
+  }).map(p => _.without(UltiSite.HatInfo.schema._schemaKeys, 'accessKey').map(key => `"${String(p[key]).replace(/"/g, '\'')}"`).join(';')).join('\n\r');
+  res.writeHead(200, { "content-type": "text/csv", "content-disposition": `attachment; filename="participants-${UltiSite.settings().hatName}.csv"` });
+  res.end(`${csv}\n\r`);
 });
