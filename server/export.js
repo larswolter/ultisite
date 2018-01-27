4822b2fcc4e1eb1a0845542cf61c331c6c82d1ac
Lars Wolter <lars.wolter@geomagic.world>
2018-01-27 16:52:39 +0100

    exporting played tournaments

---
+++ server/export.js
@@ -0,0 +1,46 @@
+import { WebApp } from 'meteor/webapp';
+import Excel from 'exceljs';
+
+
+WebApp.connectHandlers.use('/_myTournaments.xlsx', (req, resp) => {
+  if (!req.query.token) {
+    resp.writeHead(403);
+    resp.end('Param token required');
+    return;
+  }
+  const user = Meteor.users.findOne({ 'profile.downloadToken': req.query.token });
+  if (!user) {
+    resp.writeHead(403);
+    resp.end('token not valid');
+    return;
+  }
+
+  const stats = UltiSite.Statistics.findOne({ target: user._id, type: 'playedTournaments' });
+  if (!stats) {
+    resp.writeHead(404);
+    resp.end('no statistics found');
+    return;
+  }
+
+  const workbook = new Excel.Workbook();
+  workbook.creator = 'Ultisite';
+  workbook.lastModifiedBy = user.username;
+  workbook.created = new Date();
+  workbook.modified = new Date();
+
+  const sheet = workbook.addWorksheet('Gespielte Turniere', { views: [{ xSplit: 1, ySplit: 1 }] });
+  sheet.columns = [
+    { header: 'Datum', key: 'date', width: 15 },
+    { header: 'Turnier', key: 'name', width: 32 },
+    { header: 'Land', key: 'country', width: 10 },
+    { header: 'Stadt', key: 'city', width: 24 },
+    { header: 'Team', key: 'teamname', width: 24 },
+  ];
+  stats.data.forEach(entry => sheet.addRow(entry));
+  resp.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  resp.setHeader('Content-Disposition', `attachment; filename="gespielte-turniere.xlsx"`);
+  resp.writeHead(200);
+  workbook.xlsx.write(resp).then(() => {
+    resp.end();
+  });
+});
--- server/statistics.js
+++ server/statistics.js
@@ -1,108 +1,109 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
 Meteor.startup(() => {
-    UltiSite.Statistics.remove({});
+  UltiSite.Statistics.remove({});
 });
 
 Meteor.methods({
-    computeStatistics: function (userId) {
-        this.unblock();
-        let players = [];
+  computeStatistics(userId) {
+    check(userId, String);
+    this.unblock();
+    let players = [];
 
-        const teams = UltiSite.Teams.find({
-            state: 'dabei',
-            'participants': { 
-                $elemMatch: {
-                    user: userId,
-                    state: 100
-                }
-            },
-        }).map(function (team) {
-            var tournament = UltiSite.Tournaments.findOne(team.tournamentId);
-            if (!tournament)
-                return undefined;
-            if (!team)
-                return undefined;
+    const teams = UltiSite.Teams.find({
+      state: 'dabei',
+      participants: {
+        $elemMatch: {
+          user: userId,
+          state: 100,
+        },
+      },
+    }).map(function (team) {
+      const tournament = UltiSite.Tournaments.findOne(team.tournamentId);
+      if (!tournament) { return undefined; }
+      if (!team) { return undefined; }
 
-            if (Array.isArray(team.participants) && moment().isAfter(tournament.date))
-                players = players.concat(team.participants.filter(part => (part._id !== userId) && (part.state === 100)).map(function (participant) {
-                    return participant.user;
-                }));
+      if (Array.isArray(team.participants) && moment().isAfter(tournament.date)) {
+        players = players.concat(team.participants.filter(part => (part._id !== userId) && (part.state === 100)).map(function (participant) {
+          return participant.user;
+        }));
+      }
 
-            return {
-                name: tournament.name,
-                date: tournament.date,
-                teamname: team.name,
-                teamId: team._id,
-                teamImage: team.image,
-                tournamentId: tournament._id,
-                unsure: (team.state != "dabei") || (tournament.category == "Veranstaltung")
-            };
-        });
-        var played = _.sortBy(teams.filter(function (elem) {
-            if (!elem)
-                return false;
-            if (moment(elem.date).isAfter(moment()))
-                return false;
-            return true;
-        }), function (elem) {
-            return -moment(elem.date).diff(moment(), "seconds");
-        });
-        var planned = _.sortBy(teams.filter(function (elem) {
-            if (!elem)
-                return false;
-            if (moment(elem.date).isBefore(moment()))
-                return false;
-            return true;
-        }), function (elem) {
-            return moment(elem.date).diff(moment(), "seconds");
-        });
+      return {
+        name: tournament.name,
+        date: tournament.date,
+        city: tournament.address.city,
+        country: tournament.address.country,
+        teamname: team.name,
+        teamId: team._id,
+        teamImage: team.image,
+        tournamentId: tournament._id,
+        unsure: (team.state !== "dabei") || (tournament.category === "Veranstaltung"),
+      };
+    });
+    const played = _.sortBy(teams.filter(function (elem) {
+      if (!elem) { return false; }
+      if (moment(elem.date).isAfter(moment())) { return false; }
+      return true;
+    }), function (elem) {
+      return -moment(elem.date).diff(moment(), "seconds");
+    });
+    const planned = _.sortBy(teams.filter(function (elem) {
+      if (!elem) { return false; }
+      if (moment(elem.date).isBefore(moment())) { return false; }
+      return true;
+    }), function (elem) {
+      return moment(elem.date).diff(moment(), "seconds");
+    });
 
-        var stats = {
-            target: userId,
-            type: "playedTournaments",
-            data: played
-        };
-        UltiSite.Statistics.upsert({
-            target: stats.target,
-            type: stats.type
-        }, {
-                $set: {
-                    data: stats.data
-                }
-            });
-        stats = {
-            target: userId,
-            type: "plannedTournaments",
-            data: planned
-        };
-        UltiSite.Statistics.upsert({
-            target: stats.target,
-            type: stats.type
-        }, {
-                $set: {
-                    data: stats.data
-                }
-            });
-        let result = [];
-        const playerCounts = _.countBy(players, p => p);
+    let stats = {
+      target: userId,
+      type: "playedTournaments",
+      data: played,
+    };
+    UltiSite.Statistics.upsert({
+      target: stats.target,
+      type: stats.type,
+    }, {
+      $set: {
+          data: stats.data,
+        },
+    });
+    stats = {
+      target: userId,
+      type: "plannedTournaments",
+      data: planned,
+    };
+    UltiSite.Statistics.upsert({
+      target: stats.target,
+      type: stats.type,
+    }, {
+      $set: {
+          data: stats.data,
+        },
+    });
+    let result = [];
+    const playerCounts = _.countBy(players, p => p);
 
-        Object.keys(playerCounts).forEach((pc) => {
-            result.push({
-                userId: pc,
-                username: (Meteor.users.findOne(pc) || {}).username || pc,
-                count: playerCounts[pc]
-            });
-        });
-        result = _.first(_.sortBy(result, 'count').reverse(), 10);
+    Object.keys(playerCounts).forEach((pc) => {
+      result.push({
+        userId: pc,
+        username: (Meteor.users.findOne(pc) || {}).username || pc,
+        count: playerCounts[pc],
+      });
+    });
+    result = _.first(_.sortBy(result, 'count').reverse(), 10);
 
-        var top10Stats = {
-            target: userId,
-            type: "top10Players",
-            data: result
-        };
-        UltiSite.Statistics.upsert({
-            target: top10Stats.target,
-            type: top10Stats.type
-        }, top10Stats);
-    }
+    const top10Stats = {
+      target: userId,
+      type: "top10Players",
+      data: result,
+    };
+    UltiSite.Statistics.upsert({
+      target: top10Stats.target,
+      type: top10Stats.type,
+    }, top10Stats);
+  },
 
 });
