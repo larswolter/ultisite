bce220a2d0d24c1db18bff6a53cff2dbe35c9076
Lars Wolter <lars.wolter@geomagic.world>
2018-10-02 17:20:55 +0200

    fixed saving practices, fixed forms bug with date

--- server/practices.js
+++ server/practices.js
@@ -1,39 +1,27 @@
 
 
 Meteor.methods({
-  checkPracticeSheet: _.throttle(() => {
-    HTTP.get('https://sheets.googleapis.com/v4/spreadsheets/1lAiacQ1jj4ROk3BIB7auN9Rz6AFm1I4pzhzi-RdkEq8/values/B3:Z4?majorDimension=COLUMNS&key=AIzaSyBDwebvdBP1gVxFjRU5IpiDoGPVMjsuyHs',
-      (err, res) => {
-        if (res) {
-          res.data.values.forEach((practice) => {
-            const date = moment(practice[0], 'DD.MM.');
-            if (date.isBefore(moment().subtract(6, 'month'))) {
-              date.add(1, 'year');
-            }
-            const updated = UltiSite.Practices.update({
-              weekday: date.weekday(),
-              'planned.day': practice[0],
-            }, { $set: { 'planned.$.yes': practice[1] } });
-            if (!updated) {
-              UltiSite.Practices.update({
-                weekday: date.weekday(),
-              }, { $push: { planned: { day: practice[0], yes: practice[1] } } });
-            }
-          });
-        } else {
-          console.error(err);
-        }
-      });
-  }, 20000),
   updatePracticeImage(image) {
+    check(image, Object);
     console.log('Updated practices map');
+    if (!this.userId) {
+      throw new Meteor.Error('access-denied');
+    }
     UltiSite.Practices.update(image.associated[0], {
       $set: { mapImage: image._id, lastChange: new Date() },
     });
   },
   updatePractice(id, practice) {
+    check(id, Match.Maybe(String));
+    check(practice, Object);
+    if (!this.userId) {
+      throw new Meteor.Error('access-denied');
+    }
+
     if (id) {
-      UltiSite.Practices.update(id, { ...practice, lastChange: new Date() });
+      if (!practice.$set) throw new Meteor.Error('update needs to be an update');
+      practice.$set.lastChange = new Date();
+      UltiSite.Practices.update(id, practice);
     } else {
       return UltiSite.Practices.insert({ ...practice, lastChange: new Date() });
     }
