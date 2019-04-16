fa8d2be05252de0aed50c60f419544e97315ba0e
Lars Wolter <lars@larswolter.de>
2019-04-16 07:41:37 +0200

    updated service worker to catch navigations

--- private/serviceWorker.js
+++ private/serviceWorker.js
@@ -22,24 +22,27 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.setCatchHandler(({ event }) => {
+  console.log('MyCatch:', event.request.destination, event.request.mode);
+  if ((event.request.mode === 'navigation') || (event.request.mode === 'navigate')) {
+    return caches.match('/');
+  }
   switch (event.request.destination) {
   case 'image':
     return caches.match(workbox.precaching.getCacheKeyForURL(FALLBACK_IMAGE_URL));
-  case 'document':
-    return caches.match('/');
   default:
       // If we don't have a fallback, just return an error response.
     return Response.error();
   }
 });
 workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  /\.(?:png|gif|jpg|jpeg|svg|_image)$/,
   new workbox.strategies.CacheFirst({
     cacheName: 'images',
     plugins: [
       new workbox.expiration.Plugin({
         maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 Days
+        purgeOnQuotaError: true,
       }),
     ],
   })
@@ -61,6 +64,8 @@ workbox.routing.registerRoute(
   new workbox.strategies.NetworkOnly()
 );
 
+workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
+
 addEventListener('message', (event) => {
   console.log('got message', event.data);
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -69,51 +74,3 @@ addEventListener('message', (event) => {
   }
 });
 
-/*
-const CACHE_NAME = 'CURRENT_CACHE_NAME';
-
-self.addEventListener('install', function (event) {
-  console.log('SW-installing service worker...');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(
-        'FILES_TO_CACHE'
-      ).then(() => {
-        console.log('SW-installed service worker', CACHE_NAME);
-        return Promise.resolve();
-      });
-    }));
-});
-
-self.addEventListener('fetch', function (event) {
-  if (!(event.request.method === 'POST') && !(event.request.url.indexOf('/sockjs') > 0)) {
-    event.respondWith(
-      caches.match(event.request).then(function (resp) {
-        return resp || fetch(event.request);
-      }).catch(function () {
-        return caches.match('/');
-      })
-    );
-  }
-});
-
-self.addEventListener('sync', function (event) {
-  console.log('syncing....');
-});
-
-self.addEventListener('activate', function (event) {
-  const cacheWhitelist = [CACHE_NAME];
-
-  event.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('SW-removing old cache:', key);
-          return caches.delete(key);
-        }
-        return Promise.resolve();
-      }));
-    })
-  );
-});
-*/
