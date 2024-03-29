f8f0e1c6182ce319c46002a7a06b19f5d18e69de
Lars Wolter <lars@larswolter.de>
2024-03-29 18:52:45 +0100

    Wikipage edit

--- server/wikipage.js
+++ server/wikipage.js
@@ -1,43 +1,15 @@
-
-Meteor.startup(function () {
-
-    // UserStatus.events.on("connectionLogout", function (fields) {
-    //     UltiSite.WikiPages.update({
-    //         locked: fields.userId
-    //     }, {
-    //         $set: {
-    //             locked: null
-    //         }
-    //     });
-    // });
-});
+const { default: UltiSite } = require('../imports/Ultisite');
 
 Meteor.methods({
-    getWikiPageNames: function () {
-        return UltiSite.WikiPages.find({
-        }, {
-            fields: {
-                name: 1,
-                _id: 1
-            }
-        }).map(function (e) {
-            return e;
-        });
-    },
-    addWikiPageDiscussion: function(pageId,text) {
-        UltiSite.WikiPageDiscussions.insert({
-            content: text,
-            editor: this.userId,
-            editorName: Meteor.users.findOne(this.userId).username,
-            date:new Date(),
-            pageId: pageId
-        });        
-        Meteor.call('addEvent', {
-            type: 'wiki',
-            _id: pageId,
-            text: 'Neuer Diskussionsbeitrag'
-        });
+  async updateWikiPage(name, content) {
+    check(name, String);
+    check(content, String);
+    if (this.userId) {
+      const user = await Meteor.users.findOneAsync({ _id: this.userId });
+      UltiSite.WikiPages.upsertAsync(
+        { name },
+        { $set: { content, lastChange: new Date(), editor: this.userId, editorName: user.username } }
+      );
     }
+  },
 });
-
-
