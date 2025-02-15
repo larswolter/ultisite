f93a9a15bd5e944dc495df58f09ed49b8585e70a
Lars Wolter <lars@larswolter.de>
2025-02-15 20:17:38 +0100

    WIP

--- common/search.js
+++ common/search.js
@@ -1,4 +1,5 @@
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import { moment } from 'meteor/momentjs:moment';
 
 // generate search filter
 // (term1 in field1 or term1 in field2) and (term2 in field1 or term2 in field2)
@@ -26,133 +27,134 @@ function getSearchFilter(term, fields) {
   });
   return search;
 }
+export function search(term, type) {
+  if (!this.userId) {
+    return;
+  }
+
+  let res = [];
+  if (_.contains(type, 'Tournaments')) {
+    res = res.concat(
+      UltiSite.Tournaments.find(getSearchFilter(term, ['name', 'address.city', 'date']), {
+        limit: 10,
+        sort: {
+          date: -1,
+        },
+      }).map(function (elem) {
+        return {
+          name: elem.name + ' (' + moment(elem.date).format('YYYY') + ')',
+          additional: elem.address ? elem.address.city : undefined,
+          icon: 'fa-trophy',
+          isImage: false,
+          id: elem._id,
+          _id: elem._id,
+          link: FlowRouter.path('tournament', {
+            _id: elem._id,
+          }),
+        };
+      })
+    );
+  }
+
+  if (_.contains(type, 'Images')) {
+    res = res.concat(
+      UltiSite.Images.find(getSearchFilter(term, ['name', 'tags']), {
+        limit: 10,
+      }).map(function (elem) {
+        return {
+          name: elem.name,
+          icon: 'fa-picture-o',
+          isImage: true,
+          id: elem._id,
+          _id: elem._id,
+          link: FlowRouter.path('image', {
+            _id: elem._id,
+          }),
+        };
+      })
+    );
+  }
+  if (_.contains(type, 'Documents')) {
+    res = res.concat(
+      UltiSite.Documents.find(getSearchFilter(term, ['name', 'tags']), {
+        limit: 10,
+      }).map(function (elem) {
+        return {
+          name: elem.name,
+          icon: 'fa-file-o',
+          isImage: false,
+          _id: elem._id,
+          id: elem._id,
+          link: elem.url(),
+        };
+      })
+    );
+  }
+  if (_.contains(type, 'WikiPages')) {
+    res = res.concat(
+      UltiSite.WikiPages.find(getSearchFilter(term, ['name']), {
+        limit: 10,
+      }).map(function (elem) {
+        return {
+          name: elem.name,
+          icon: 'fa-book',
+          isImage: false,
+          id: elem._id,
+          _id: elem._id,
+          link: FlowRouter.path('wikipage', {
+            name: elem._id,
+          }),
+        };
+      })
+    );
+  }
+  if (_.contains(type, 'Blogs')) {
+    res = res.concat(
+      UltiSite.Blogs.find(getSearchFilter(term, ['title']), {
+        limit: 10,
+      }).map(function (elem) {
+        return {
+          name: elem.title,
+          icon: 'fa-newspaper-o',
+          isImage: false,
+          id: elem._id,
+          _id: elem._id,
+          link: FlowRouter.path('blog', {
+            title: elem.title,
+          }),
+        };
+      })
+    );
+  }
+  if (_.contains(type, 'Users')) {
+    const search = getSearchFilter(term, ['profile.name', 'profile.surname', 'emails.address', 'username']);
+
+    res = res.concat(
+      Meteor.users
+        .find(search, {
+          limit: 10,
+        })
+        .map(function (elem) {
+          return {
+            name: elem.username + ' (' + elem.profile.name + ' ' + elem.profile.surname + ')',
+            sex: elem.profile.sex,
+            isImage: false,
+            icon: 'fa-user',
+            id: elem._id,
+            _id: elem._id,
+            link: FlowRouter.path('user', {
+              _id: elem._id,
+            }),
+          };
+        })
+    );
+  }
+  return res;
+}
 
 Meteor.startup(function () {
   _.extend(UltiSite, {
-    search(term, type) {
-      if (!this.userId) {
-        return;
-      }
-
-      let res = [];
-      if (_.contains(type, 'Tournaments')) {
-        res = res.concat(
-          UltiSite.Tournaments.find(getSearchFilter(term, ['name', 'address.city', 'date']), {
-            limit: 10,
-            sort: {
-              date: -1,
-            },
-          }).map(function (elem) {
-            return {
-              name: elem.name + ' (' + moment(elem.date).format('YYYY') + ')',
-              additional: elem.address ? elem.address.city : undefined,
-              icon: 'fa-trophy',
-              isImage: false,
-              id: elem._id,
-              _id: elem._id,
-              link: FlowRouter.path('tournament', {
-                _id: elem._id,
-              }),
-            };
-          })
-        );
-      }
-
-      if (_.contains(type, 'Images')) {
-        res = res.concat(
-          UltiSite.Images.find(getSearchFilter(term, ['name', 'tags']), {
-            limit: 10,
-          }).map(function (elem) {
-            return {
-              name: elem.name,
-              icon: 'fa-picture-o',
-              isImage: true,
-              id: elem._id,
-              _id: elem._id,
-              link: FlowRouter.path('image', {
-                _id: elem._id,
-              }),
-            };
-          })
-        );
-      }
-      if (_.contains(type, 'Documents')) {
-        res = res.concat(
-          UltiSite.Documents.find(getSearchFilter(term, ['name', 'tags']), {
-            limit: 10,
-          }).map(function (elem) {
-            return {
-              name: elem.name,
-              icon: 'fa-file-o',
-              isImage: false,
-              _id: elem._id,
-              id: elem._id,
-              link: elem.url(),
-            };
-          })
-        );
-      }
-      if (_.contains(type, 'WikiPages')) {
-        res = res.concat(
-          UltiSite.WikiPages.find(getSearchFilter(term, ['name']), {
-            limit: 10,
-          }).map(function (elem) {
-            return {
-              name: elem.name,
-              icon: 'fa-book',
-              isImage: false,
-              id: elem._id,
-              _id: elem._id,
-              link: FlowRouter.path('wikipage', {
-                name: elem._id,
-              }),
-            };
-          })
-        );
-      }
-      if (_.contains(type, 'Blogs')) {
-        res = res.concat(
-          UltiSite.Blogs.find(getSearchFilter(term, ['title']), {
-            limit: 10,
-          }).map(function (elem) {
-            return {
-              name: elem.title,
-              icon: 'fa-newspaper-o',
-              isImage: false,
-              id: elem._id,
-              _id: elem._id,
-              link: FlowRouter.path('blog', {
-                title: elem.title,
-              }),
-            };
-          })
-        );
-      }
-      if (_.contains(type, 'Users')) {
-        const search = getSearchFilter(term, ['profile.name', 'profile.surname', 'emails.address', 'username']);
-        const self = this;
-        res = res.concat(
-          Meteor.users
-            .find(search, {
-              limit: 10,
-            })
-            .map(function (elem) {
-              return {
-                name: elem.username + ' (' + elem.profile.name + ' ' + elem.profile.surname + ')',
-                sex: elem.profile.sex,
-                isImage: false,
-                icon: 'fa-user',
-                id: elem._id,
-                _id: elem._id,
-                link: FlowRouter.path('user', {
-                  _id: elem._id,
-                }),
-              };
-            })
-        );
-      }
-      return res;
-    },
+    search,
   });
   Meteor.methods({
     search: UltiSite.search,
