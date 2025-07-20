053df856316a0b2c9635f36e708da7d234921390
Lars Wolter <lars@larswolter.de>
2025-07-20 21:26:34 +0200

    removing service workers

--- client/offline.js
+++ client/offline.js
@@ -293,57 +293,7 @@ if (!Package.appcache) {
 }
 
 if ('serviceWorker' in navigator) {
-  console.log('service-worker: service worker available');
-  if (Meteor.settings && Meteor.settings.public && Meteor.settings.public.disableGMServiceWorker) {
-    // if we disable service worker, force unregistration of them
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-      registrations.forEach((registration) => registration.unregister());
-    });
-    return;
-  }
-
-  const wb = new Workbox(Meteor.absoluteUrl('sw.js'));
-  let retry;
-  wb.addEventListener('statechange', (evt) => {
-    console.log('service-worker: Active state:' + evt.target.state);
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
   });
-  wb.addEventListener('waiting', (event) => {
-    console.log('service-worker: Waiting service worker found');
-    if (event.sw) {
-      console.log('service-worker: sending command to skip');
-      messageSW(event.sw, { type: 'SKIP_WAITING' });
-    }
-  });
-  wb.addEventListener('externalwaiting', (event) => {
-    console.log('service-worker: External Waiting service worker found');
-    if (event.sw) {
-      console.log('service-worker: sending command to skip');
-      messageSW(event.sw, { type: 'SKIP_WAITING' });
-    }
-  });
-  let canMigrate = false;
-  wb.addEventListener('controlling', (event) => {
-    console.log('service-worker: is controlling, retrying migration');
-    canMigrate = true;
-    if (retry) retry();
-    else window.location.reload();
-  });
-  wb.addEventListener('externalactivated', (event) => {
-    console.log('service-worker: is external activated, retrying migration');
-    canMigrate = true;
-    if (retry) retry();
-    else window.location.reload();
-  });
-  Reload._onMigrate((r) => {
-    if (canMigrate) return [canMigrate];
-    console.log('service-worker: force updating service worker...');
-    wb.update();
-    retry = r;
-    localForage.setItem('ultisiteOfflineLastSync', {});
-    return false;
-  });
-  wb.register();
-  console.log('service-worker: registered service worker');
-} else {
-  console.log('service-worker: service worker not available');
 }
